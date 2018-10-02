abc57a2c4fc1aeb2e5f7b8ac7fd3b97592669ab4
= <peterhjhan@gmail.com>
2018-10-02 23:23:08 +0000

    committing after finishing template engine

--- assignments/tinyapp/express_server.js
+++ assignments/tinyapp/express_server.js
@@ -13,9 +13,9 @@ var urlDatabase = {
 //   res.send("Hello!");
 // });
 
-for(let values in urlDatabase){
-  console.log(values + urlDatabase[values]); 
-}
+// for(let values in urlDatabase){
+//   console.log(values + urlDatabase[values]); 
+// }
 
 
 
@@ -25,14 +25,15 @@ for(let values in urlDatabase){
 
 
 
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id,
+  longURL : urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-});
-
-app.get("/hello", (req, res) => {
-  let templateVars = { greeting: 'Hello World!' };
-  res.render("hello_world", templateVars);
 });
 
 app.listen(PORT, () => {
